de695563168ba11e17e3694f72321bfccb178cf0
zsychina <siyuan100@foxmail.com>
2025-01-19 22:48:30 +0800

    add main

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ from test import DistributedTester
 import gymnasium as gym
 import json
 
+import matplotlib.pyplot as plt
 
 
 with open('targets.json', 'r') as f:
     targets = json.load(f)
     
-print(targets['trains'])
-
 
 
 for target in targets['trains']:
-    pass
+    trainer = DistributedTrainer(
+        env_name=target['environment'],
+        algo_name=target['algorithm'],
+        workers_num=5
+    )
+    
+    trainer.train(episodes=5)
+    
+    # trainer.algo.save(f'./ckpt/{target["env_name"]}')
+    
+    print(f'Training for {target["environment"]} is done')
+    
+    plt.plot(trainer.episode_return_mean())
+    plt.savefig(f'./assets/{target["environment"]}.png')
+    
+    
+    del trainer
 
